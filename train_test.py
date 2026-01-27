14d12de78b525c094d540bfe55aeab689c2ab20e
stalris <thestarsmyhammer@gmail.com>
2026-01-27 12:27:38 -0500

    TrainingSettingChanged

--- train_test.py
+++ train_test.py
@@ -2,14 +2,16 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # 1. Load the model
-    # It will download yolo11n-pose.pt automatically if you don't have it
-    model = YOLO('yolo11n-pose.pt')
+    # It will download yolo26n-pose.pt automatically if you don't have it
+    model = YOLO('yolo26n-pose.pt')
 
     # 2. Train the model
     results = model.train(
         data='judo_pose.yaml',   # Path to your config file
-        epochs=100,               # Quick test
+        epochs=200,               # Quick test
         imgsz=640,               # Standard image size
         project='runs',          # Folder to save results
-        name='dry_run'           # Folder name for this specific run
+        name='dry_run',           # Folder name for this specific run
+        workers=6,
+        batch=64
     )
