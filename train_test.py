32a29ec119eb10e94d7236bb81ecc107b72ed080
FranciscoJ201 <f.j.j8657@gmail.com>
2026-02-04 11:01:10 -0500

    TrainingUpdate

--- train_test.py
+++ train_test.py
@@ -1,17 +1,19 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    # 1. Load the model
-    # It will download yolo26n-pose.pt automatically if you don't have it
-    model = YOLO('yolo26n-pose.pt')
+    
+    model = YOLO('yolo26x-pose.pt')
 
     # 2. Train the model
     results = model.train(
-        data='judo_pose.yaml',   # Path to your config file
-        epochs=200,               # Quick test
-        imgsz=640,               # Standard image size
-        project='runs',          # Folder to save results
-        name='dry_run',           # Folder name for this specific run
-        workers=6,
-        batch=64
-    )
+    data='judo_pose.yaml',
+    epochs=150,          # Reduced from 200 because it learns fast
+    patience=20,         # STRICT early stopping. If it doesn't improve for 20 epochs, kill it.
+    batch=16,             # You likely need to lower this further for Large
+    workers=4,
+    imgsz=640,
+    dropout=0.1,         # CRITICAL: Keep this to fight the overfitting we saw in Medium
+    augment=True,         # Keep this on
+    project = 'Largest',
+    name = 'yolo26X-pose-judo'
+)
